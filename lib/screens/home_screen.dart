bb2cc3e67765b9435631f4e7f34034cf77842283
anas <anas33ezz@gmail.com>
2025-12-13 17:22:18 +0200

    home screen start

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(leading: const Text('Hello Anas Have A Nice Day')),
+      body: Container(),
+    );
   }
 }
