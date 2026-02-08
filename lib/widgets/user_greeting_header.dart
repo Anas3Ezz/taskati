d2703a8c083ff8da9c8a9c9d6297b20f7c457ae7
anas <anas33ezz@gmail.com>
2026-02-08 18:07:11 +0200

    handling the profile pic

--- lib/widgets/user_greeting_header.dart
+++ lib/widgets/user_greeting_header.dart
@@ -1,14 +1,26 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:taskati/constant/app_strings.dart';
 import 'package:taskati/models/user_model.dart';
+import 'package:taskati/screens/auth_screen.dart';
 import 'package:taskati/theme/texts_styles.dart';
 
 class UserGreetingHeader extends StatelessWidget {
   const UserGreetingHeader({super.key, required this.user});
   final UserModel? user;
+  String formatDisplayName(String? name) {
+    if (name == null || name.isEmpty) {
+      return "?"; // Fallback character
+    }
+    return name[0].toUpperCase();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final String displayName = formatDisplayName(user?.name);
+    final String firstLetter = displayName.isNotEmpty ? displayName[0] : "U";
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5.0),
       child: Row(
@@ -17,7 +29,7 @@ class UserGreetingHeader extends StatelessWidget {
             TextSpan(
               children: [
                 TextSpan(
-                  text: 'Hi,${user?.name ?? ''}',
+                  text: 'Hi ${user?.name ?? ''}',
                   style: AppTextStyles().s22deepPurpleAccentWeight500,
                 ),
                 const TextSpan(
@@ -30,7 +42,31 @@ class UserGreetingHeader extends StatelessWidget {
           const Spacer(),
           CircleAvatar(
             radius: 30,
-            backgroundImage: Image.file(File(user?.image ?? '')).image,
+            backgroundColor: Colors.deepPurpleAccent,
+            backgroundImage: (user?.image != null && user!.image.isNotEmpty)
+                ? FileImage(File(user!.image))
+                : null,
+            child: (user?.image == null || user!.image.isEmpty)
+                ? Text(
+                    formatDisplayName(user?.name),
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  )
+                : null,
+          ),
+          IconButton(
+            onPressed: () {
+              Hive.box<UserModel>(AppStrings.userBox).clear();
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(builder: (context) => const AuthScreen()),
+                (route) => false,
+              );
+            },
+            icon: const Icon(Icons.key),
           ),
         ],
       ),
